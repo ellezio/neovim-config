f9edd015df24e6bf89cefdc25b6f437f6477de48
Paweł Lendzion <pawellendzion01@gmail.com>
2024-05-20 12:31:55 +0200

    Remove Aerial, add outline

---
+++ lua/plugins/outline.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"hedyhli/outline.nvim",
+		lazy = true,
+		cmd = { "Outline", "OutlineOpen" },
+		keys = {
+			{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+		},
+		opts = {
+			outline_window = {
+				show_cursorline = true,
+				hide_cursor = true,
+			},
+		},
+	},
+}
