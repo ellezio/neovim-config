dd915ae81e1ed6813b49e8fa3051a917980a4363
Paweł Lendzion <pawellendzion01@gmail.com>
2024-07-15 23:00:50 +0200

    Update 'which-key' config

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -6,26 +6,25 @@ return {
 			local wk = require('which-key')
 
 			wk.setup({
-				window = {
+				win = {
 					border = 'rounded'
+				},
+				icons = {
+					rules = false
 				}
 			})
 
-			wk.register({
-				c = { name = '[C]ode', _ = 'which_key_ignore' },
-				d = { name = '[D]ocument', _ = 'which_key_ignore' },
-				g = { name = '[G]it', _ = 'which_key_ignore' },
-				h = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-				r = { name = '[R]ename', _ = 'which_key_ignore' },
-				s = { name = '[S]earch', _ = 'which_key_ignore' },
-				t = { name = '[T]oggle', _ = 'which_key_ignore' },
-				w = { name = '[W]orkspace', _ = 'which_key_ignore' },
-				n = { name = '[N]eotree', _ = 'which_key_ignore' },
-			}, { prefix = '<leader>' })
-
-			wk.register({
-				h = { 'Git [H]unk' },
-			}, { mode = 'v', prefix = '<leader>' })
+			wk.add({
+				{ "<leader>c", group = '[C]ode' },
+				{ "<leader>d", group = '[D]ocument' },
+				{ "<leader>g", group = '[G]it' },
+				{ "<leader>h", group = 'Git [H]unk', mode = { "n", "v" } },
+				{ "<leader>r", group = '[R]ename' },
+				{ "<leader>s", group = '[S]earch' },
+				{ "<leader>t", group = '[T]oggle' },
+				{ "<leader>w", group = '[W]orkspace' },
+				{ "<leader>n", group = '[N]eotree' },
+			})
 		end
 	},
 }
