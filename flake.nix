359645775a23272a95055a5751f31eaeb18347f6
Paweł Lendzion <pawellendzion01@gmail.com>
2024-09-01 19:11:49 +0200

    Add option to add plugins

--- flake.nix
+++ flake.nix
@@ -8,33 +8,47 @@
   outputs = inputs@{ nixpkgs, ... }:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; };
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [
+          (final: prev: {
+            # Add custom plugins here
+            vimPlugins = prev.vimPlugins // {
+              # Example of adding plugins:
+              #
+              # todo: figure out how 'buildNeovimPlugin' works
+              # <pluginName> = prev.vimUtils.buildVimPlugin {
+              #   name = "<pluginName>";
+              #   src = <pluginSrc>;
+              # };
+            };
+          })
+        ];
+      };
 
+      plugins = [
+        { plugin = pkgs.vimPlugins.catppuccin-nvim; }
+      ];
 
-      neovimConfig = (pkgs.neovimUtils.makeNeovimConfig { });
-
-      init = pkgs.writeText "init.lua" ''
-        print('It works')
-      '';
-
-      extraWrapperArgs = [ "--add-flags" "-u ${init}" ];
+      neovimConfig = (pkgs.neovimUtils.makeNeovimConfig { inherit plugins; });
 
       finalePackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
         neovimConfig
         // {
-          wrapRc = false;
-          wrapperArgs = neovimConfig.wrapperArgs ++ extraWrapperArgs;
+          wrapRC = false;
+          wrapperArgs = neovimConfig.wrapperArgs ++ [ "--add-flags" ''-u ${./init.lua}'' ];
         }
       );
 
       nvim = pkgs.symlinkJoin {
         name = "my-nvim";
         paths = [
-          finalePackage
+          (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
+            finalePackage
+          ))
         ];
         meta.mainProgram = "nvim";
       };
-
     in
     {
       packages.${system}.default = nvim;
