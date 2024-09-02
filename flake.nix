56f4db9ab87cc4f89d1a5fd74eced1e3c63c90c9
Paweł Lendzion <pawellendzion01@gmail.com>
2024-09-02 22:19:19 +0200

    Add Telescope, and custom options

--- flake.nix
+++ flake.nix
@@ -26,19 +26,32 @@
         ];
       };
 
-      plugins = [
-        { plugin = pkgs.vimPlugins.catppuccin-nvim; }
+      plugins = with pkgs.vimPlugins; [
+        { plugin = catppuccin-nvim; }
+        { plugin = telescope-nvim; }
+        { plugin = plenary-nvim; }
+        { plugin = telescope-fzf-native-nvim; }
+        { plugin = telescope-ui-select-nvim; }
       ];
 
       neovimConfig = (pkgs.neovimUtils.makeNeovimConfig { inherit plugins; });
 
-      finalePackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped (
-        neovimConfig
-        // {
-          wrapRC = false;
-          wrapperArgs = neovimConfig.wrapperArgs ++ [ "--add-flags" ''-u ${./init.lua}'' ];
-        }
-      );
+      # TODO Separate lua files
+      luaRcContent = ''
+        -- ### Options ###
+        ${builtins.readFile ./lua/options.lua}
+
+        -- ### Telescope ###
+        ${builtins.readFile ./lua/plugins/telescope.lua}
+
+        -- ### Init ###
+        ${builtins.readFile ./init.lua}
+      '';
+
+      finalePackage = pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped
+        (
+          neovimConfig // { inherit luaRcContent; }
+        );
 
       nvim = pkgs.symlinkJoin {
         name = "my-nvim";
@@ -47,10 +60,10 @@
             finalePackage
           ))
         ];
-        meta.mainProgram = "nvim";
+        meta. mainProgram = "nvim";
       };
     in
     {
-      packages.${system}.default = nvim;
+      packages.${ system}. default = nvim;
     };
 }
